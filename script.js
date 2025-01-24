0761d876cf7cddaec70b003ebea9dada6af83d29
felirambo <191887565+felirambo@users.noreply.github.com>
2025-01-24 10:37:17 -0300

    Added UI to play rock, paper and scisor game

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+let computerScore = 0;
+let humanScore = 0;
+
 let rockBtn = document.querySelector("#rock-btn");
 let paperBtn = document.querySelector("#paper-btn");
 let scisorBtn = document.querySelector("#scisor-btn");
+let scoreDisplay = document.querySelector("#score-display");
 
 rockBtn.addEventListener("click", playRound);
 paperBtn.addEventListener("click", playRound);
 scisorBtn.addEventListener("click", playRound);
 
 let selections = document.querySelector("#selections");
-let winnerField = document.querySelector("#winner-field");
-let scoreDisplay = document.querySelector("#score-display");
 
 function getComputerChoice() {
     let randomNumber = Math.random();
@@ -28,49 +30,97 @@ function getHumanChoice(e) {
     return e.target.textContent.toLowerCase();
 }
 
+function displayScore(winner) {
+
+    if (winner == "human") {
+        humanScore++;
+        scoreDisplay.innerText = "Player win this round \n";
+    }
+    else if (winner == "computer") {
+        computerScore++; 
+        scoreDisplay.innerText = "Computer win this round \n";
+    }
+    else if (winner == "draw") scoreDisplay.innerText = "It's a draw. \n"
+
+    scoreDisplay.innerText += `Computer Score: ${computerScore} \n Human Score: ${humanScore}`;
+    
+    if (humanScore == 5) {
+        scoreDisplay.innerText += `\n Human win the game!`;
+        endGame();
+    }
+    else if (computerScore == 5) {
+        scoreDisplay.innerText += `\n Computer win the game!`;
+        endGame();
+    }
+}
+
+function endGame() {
+    rockBtn.removeEventListener("click", playRound);
+    paperBtn.removeEventListener("click", playRound);
+    scisorBtn.removeEventListener("click", playRound);
+
+    let restartBtn = document.createElement("button");
+    restartBtn.innerText = "Restart Game";
+    restartBtn.addEventListener("click", restartGame);
+
+    scoreDisplay.appendChild(restartBtn);
+}
+
+function restartGame() {
+
+    computerScore = 0;
+    humanScore = 0;
+
+    rockBtn.addEventListener("click", playRound);
+    paperBtn.addEventListener("click", playRound);
+    scisorBtn.addEventListener("click", playRound);
+
+    scoreDisplay.innerText = `Computer Score: ${computerScore} \n Human Score: ${humanScore}`;
+    selections.innerText = "";
+}
+
+
 function playRound(e) {
     let computerChoice = getComputerChoice();
     let humanChoice = getHumanChoice(e);
+    let winner = "";
 
     selections.textContent = `Player choose ${humanChoice}, computer choose ${computerChoice}`;
 
-    let winnerFieldDisplay1 = "Computer wins!";
-    let winnerFieldDisplay2 = "Player wins!";
-    let winnerFieldDisplay3 = "It's a draw!";
-
     if (computerChoice == "rock") {
         if (humanChoice == "scisor") {
-            winnerField.textContent = winnerFieldDisplay1;
+            winner = "computer";
         }
         else if (humanChoice == "rock") {
-            winnerField.textContent = winnerFieldDisplay3;
+            winner = "draw"
         }
         else {
-            winnerField.textContent = winnerFieldDisplay2;
+            winner = "human";
         }
     }
     if (computerChoice == "paper") {
         if (humanChoice == "scisor") {
-            winnerField.textContent = winnerFieldDisplay2;
+            winner = "human"
         }
         else if (humanChoice == "rock") {
-            winnerField.textContent = winnerFieldDisplay1;
+            winner = "computer";
         }
         else {
-            winnerField.textContent = winnerFieldDisplay3;
+            winner = "draw";
         }
     }
     if (computerChoice == "scisor") {
         if (humanChoice == "scisor") {
-            winnerField.textContent = winnerFieldDisplay3;
+            winner = "draw";
         }
         else if (humanChoice == "rock") {
-            winnerField.textContent = winnerFieldDisplay2;
+            winner = "human";
         }
         else {
-            winnerField.textContent = winnerFieldDisplay1;
+            winner = "computer";
         }
     }
+    displayScore(winner);
     return
 }
 
